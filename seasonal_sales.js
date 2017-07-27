c04cd2739d71328558e440efab595f57b6d827b3
Bryon Larrance <beelarr@Bryons-MacBook-Pro.local>
2017-07-27 15:58:23 -0500

    JS built ready for html render

--- seasonal_sales.js
+++ seasonal_sales.js
@@ -1,3 +1,54 @@
 /**
  * Created by beelarr on 7/27/17.
  */
+
+    var Store_Items = (
+        function (){
+            let items = []
+            let categories = []
+
+            return {
+                load_items: function () {
+                    var loader = new XMLHttpRequest();
+                    loader.addEventListener('load', function () {
+                        items = JSON.parse(this.responseText);
+                        item_loader(items);
+                    });
+                    loader.open('GET', 'products.json')
+                    loader.send();
+                },
+
+                load_categories: function () {
+                    var cat_loader = new XMLHttpRequest();
+                    cat_loader.addEventListener('load', function () {
+                        categories = JSON.parse(this.responseText);
+                        category_loader(categories);
+                    });
+                    cat_loader.open('GET', categories.json)
+                    cat_loader.send();
+
+                }
+            }
+        }
+)()
+
+
+function item_loader(items) {
+            console.log(items.products[0].name)
+    document.querySelector('.products').innerHTML = items.products.name.join(', ')
+
+}
+
+function category_loader() {
+    let winter = document.querySelector('#winter')
+    let autumn = document.querySelector('#autumn')
+    let spring = document.querySelector('#spring')
+    let select = document.querySelector('option')
+
+    console.log(winter.value)
+
+
+}
+
+Store_Items.load_items(item_loader);
+Store_Items.load_categories(category_loader);
